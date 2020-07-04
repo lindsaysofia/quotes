f3dca66fa859fd87cbd63debd167708d41c1ec7b
Lindsay Lopez <lopez95@berkeley.edu>
2020-07-03 20:09:10 -0700

    added quotes array

---
+++ index.js
@@ -0,0 +1,22 @@
+const quotes = [
+  {
+    text:"Hazlo. Hazlo. No digas: &lt;&lt;No puedo hacerlo&gt;&gt;. ¡Hazlo!",
+    author: "Desconocido"
+  },
+  {
+    text:"You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis"
+  },
+  {
+    text:"Si vas a cometer errores, que sean nuevos.",
+    author: "Desconocido"
+  },
+  {
+    text:"Know that the world is an inherently beautiful place.",
+    author: "Unknown"
+  },
+  {
+    text:"La vida es un 10% lo que haces y un 90% cómo te la tomas.",
+    author: "Irving Berlin"
+  }
+];
