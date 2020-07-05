1cfe8dd79b39c648b15105d1523772d0a6f6a61f
Lindsay Lopez <lopez95@berkeley.edu>
2020-07-05 15:50:38 -0700

    completed functionality of buttons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const quotes = [
+const QUOTES = [
   {
-    text:"Hazlo. Hazlo. No digas: &lt;&lt;No puedo hacerlo&gt;&gt;. ¡Hazlo!",
+    text:"Hazlo. Hazlo. No digas: <<No puedo hacerlo>>. ¡Hazlo!",
     author: "Desconocido"
   },
   {
@@ -20,3 +20,25 @@ const quotes = [
     author: "Irving Berlin"
   }
 ];
+
+const quote = document.querySelector('.quote');
+const quoteText = document.querySelector('.quote__text');
+const quoteAuthor = document.querySelector('.quote__author');
+let index = 0;
+
+quote.addEventListener('click', event => {
+  switch (event.target.id) {
+    case "left":
+      index = (index - 1 === -1 ? QUOTES.length - 1 : index - 1);
+      break;
+    case "right":
+      index = (index + 1 === QUOTES.length ? 0 : index + 1);
+      break;
+    case "surprise":
+      index = Math.floor(Math.random() * 5);  
+      break;
+  }
+  let newQuote = QUOTES[index % QUOTES.length];
+  quoteText.innerText = newQuote.text;
+  quoteAuthor.innerText = newQuote.author;
+});
